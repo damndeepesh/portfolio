b003c35d67e3ba32f827603350c7d281524ada76
Deepesh Gupta <damndeepesh1910@gmail.com>
2025-10-01 12:10:07 +0530

    Add minimal blog-list.js to prevent build errors

---
+++ blog-list.js
@@ -0,0 +1,10 @@
+/**
+ * Blog list for portfolio website
+ * This file provides blog functionality for the website
+ */
+
+// Initialize empty blog list
+window.BLOG_LIST = [];
+
+// Log that blog list is loaded
+console.log('📚 Blog list loaded (empty)');
