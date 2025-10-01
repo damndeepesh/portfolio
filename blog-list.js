0a749cec1fd78e451909e81dbf0aeec17558516a
Deepesh Gupta <damndeepesh1910@gmail.com>
2025-10-01 12:11:09 +0530

    Make blog-list.js compatible with both Node.js and browser environments

--- blog-list.js
+++ blog-list.js
@@ -3,8 +3,16 @@
  * This file provides blog functionality for the website
  */
 
-// Initialize empty blog list
-window.BLOG_LIST = [];
-
-// Log that blog list is loaded
-console.log('📚 Blog list loaded (empty)');
+// Check if we're in a browser environment
+if (typeof window !== 'undefined') {
+    // Initialize empty blog list for browser
+    window.BLOG_LIST = [];
+    
+    // Log that blog list is loaded
+    console.log('📚 Blog list loaded (empty)');
+} else {
+    // Node.js environment - just export an empty object
+    module.exports = {
+        BLOG_LIST: []
+    };
+}
